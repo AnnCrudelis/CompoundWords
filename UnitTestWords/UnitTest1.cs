4890e747ec58f3f7de686b857c9341ee0d824f00
AnnCrudelis <ann.crudelis@gmail.com>
2021-12-26 23:58:08 +0600

    Подправила тесты

--- UnitTestWords/UnitTest1.cs
+++ UnitTestWords/UnitTest1.cs
@@ -12,56 +12,56 @@ namespace UnitTestWords
         public void TestNotAllowedToSplit()
         {
             string original = "Psychologie";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.Equal("Psychologie", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.Equal("psychologie", result.ToLower());
         }
 
         [Fact]
         public void TestAllowedToSplit()
         {
             string original = "Krankenhaus";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.Equal("Kranken haus", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.Equal("kranken haus", result.ToLower());
         }
 
         [Fact]
         public void TestWrongAllowedToSplit()
         {
             string original = "Krankenhaus";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.NotEqual("Krank en haus ", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.NotEqual("krank en haus ", result.ToLower());
         }
 
         [Fact]
         public void TestNotInDictionary()
         {
             string original = "123";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.Equal("123 ", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.Equal("123", result.ToLower());
         }
 
         [Fact]
         public void TestIsEmpty()
         {
             string original = "";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.Equal("", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.Equal("", result.ToLower());
         }
 
         [Fact]
         public void TestAlreadySplited()
         {
             string original = "Farn Fasan";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.Equal("Farn Fasan", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.Equal("farn fasan", result.ToLower());
         }
 
         [Fact]
         public void TestIsMultiWord()
         {
             string original = "Kranken haus";
-            var result = Program.SomeAlgoritm(dictionary, original);
-            Assert.NotEqual("Krank en haus", result);
+            var result = Program.SplitWord(dictionary, original);
+            Assert.NotEqual("krank en haus", result.ToLower());
         }
     }
 }
