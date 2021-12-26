2d92274c6b07aa79f5a14ad366b8a499eac4836d
AnnCrudelis <ann.crudelis@gmail.com>
2021-12-27 00:04:59 +0600

    Реализовала метод разбивания слов на составные части. Необходима доработка

--- CompoundWords/Program.cs
+++ CompoundWords/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Resources;
 using System.Xml.Linq;
 
@@ -33,6 +34,11 @@ namespace CompoundWords
                 Task<string[]> originalFile = Task.Run(() => File.ReadAllLinesAsync(originalFilePath));
                 dictionary.Wait();
                 originalFile.Wait();
+                Console.WriteLine(originalFile.IsCompleted);
+                for (int i = 0; i < originalFile.Result.Length; i++)
+                {
+                    Console.WriteLine(SplitWord(dictionary.Result,originalFile.Result[i]));
+                }
             }
             catch (Exception ex)
             {
@@ -41,9 +47,36 @@ namespace CompoundWords
 
         }
 
-        public static string SomeAlgoritm(string[] dict, string original)
+        public static string SplitWord(string[] dict, string original)
         {
-            return string.Empty;
+            List<string> Lexemes = new List<string> { };
+            original = original.ToLower();
+            for (int i = 0; i < dict.Length; i++)
+            {
+                dict[i] = dict[i].ToLower();
+                if (original.Contains(dict[i]))
+                {
+                    Lexemes.Add(dict[i]);
+                }
+            }
+
+            if (Lexemes != null)
+            {
+                List<string> list = new List<string>();
+                foreach (string lexeme in Lexemes)
+                {
+                    var pair1 = Lexemes?.Where(x => x + lexeme == original).FirstOrDefault();
+                    if(pair1 != null)
+                    {
+                        list.Add(pair1 + " " + lexeme);
+                    }
+                }
+                if(list?.Count > 0)
+                {
+                    return list.FirstOrDefault();
+                }
+            }
+            return original;
         }
 
     }
