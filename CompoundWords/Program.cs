f92963619114ad5564943fadd53b6fe04598d726
AnnCrudelis <ann.crudelis@gmail.com>
2021-12-27 00:06:42 +0600

    Добавила комментарий на будущее

--- CompoundWords/Program.cs
+++ CompoundWords/Program.cs
@@ -49,23 +49,25 @@ namespace CompoundWords
 
         public static string SplitWord(string[] dict, string original)
         {
-            List<string> Lexemes = new List<string> { };
+            //TODO: метод разбивает всего на 2 составные части. Необходимо, чтобы он мог разбивать на большее количество частей, если это возможно
+            //TODO: но учитывать условие из тз: "При реализации алгоритма, следует брать из словаря, максимально “длинное” слово, насколько это возможно."
+            List<string> lexemes = new List<string> { };
             original = original.ToLower();
             for (int i = 0; i < dict.Length; i++)
             {
                 dict[i] = dict[i].ToLower();
                 if (original.Contains(dict[i]))
                 {
-                    Lexemes.Add(dict[i]);
+                    lexemes.Add(dict[i]);
                 }
             }
 
-            if (Lexemes != null)
+            if (lexemes != null)
             {
                 List<string> list = new List<string>();
-                foreach (string lexeme in Lexemes)
+                foreach (string lexeme in lexemes)
                 {
-                    var pair1 = Lexemes?.Where(x => x + lexeme == original).FirstOrDefault();
+                    var pair1 = lexemes?.Where(x => x + lexeme == original).FirstOrDefault();
                     if(pair1 != null)
                     {
                         list.Add(pair1 + " " + lexeme);
